9860e89ebe963fc032c9fecd78638a6a7aa96f2f
Paulius B <paulspremium@gmail.com>
2021-08-20 20:11:38 +0300

    API Post

--- public/js/login.js
+++ public/js/login.js
@@ -4,10 +4,8 @@ const formMessageDOM = document.querySelector('.form-messages');
 const pFormMessageDOM = formMessageDOM.querySelector('.message');
 const closeMessageDOM = formMessageDOM.querySelector('.close');
 const formDOM = document.querySelector('.form');
-const usernameDOM = document.getElementById('username');
 const emailDOM = document.getElementById('email');
 const passDOM = document.getElementById('pass');
-const repassDOM = document.getElementById('repass');
 const submitDOM = document.querySelector('button');
 
 function showMessage(state, msg) {
@@ -28,25 +26,14 @@ function submitFormInfo(e) {
 
     const minimumPasswordLength = 8;
 
-    const username = usernameDOM.value;
     const email = emailDOM.value;
     const pass = passDOM.value;
-    const repass = repassDOM.value;
 
-    if (username === '') {
-        return showMessage('error', '"Username" negali buti tuscias');
-    }
     if (email === '') {
         return showMessage('error', '"Email" negali buti tuscias');
     }
     if (pass.length < minimumPasswordLength) {
         return showMessage('error', `"Password" negali buti trumpesnis nei ${minimumPasswordLength} simboliai`);
-    }
-    if (repass.length < minimumPasswordLength) {
-        return showMessage('error', `"Repeat password" negali buti trumpesnis nei ${minimumPasswordLength} simboliai`);
-    }
-    if (pass !== repass) {
-        return showMessage('error', 'Nesutampa "Password" ir "Repeat password"');
     }
 
     closeMessage();
@@ -54,7 +41,7 @@ function submitFormInfo(e) {
         method: 'POST',
         headers: {},
         endpoint: 'api/users',
-        data: { username, email, password: pass }
+        data: { email, password: pass }
     }, responseAction);
 }
 
